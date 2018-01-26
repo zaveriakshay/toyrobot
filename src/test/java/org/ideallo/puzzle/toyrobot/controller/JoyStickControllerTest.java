8efb8793fd1e3f1f1f5eab7260e5bcf959503d6d
zaveriakshay <zaveri.akshay@gmail.com>
2018-01-27 00:25:09 +0530

    COMMIT:26012018:Commits to work todays work

--- src/test/java/org/ideallo/puzzle/toyrobot/controller/JoyStickControllerTest.java
+++ src/test/java/org/ideallo/puzzle/toyrobot/controller/JoyStickControllerTest.java
@@ -1,35 +1,24 @@
 package org.ideallo.puzzle.toyrobot.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.spi.json.GsonJsonProvider;
-import org.ideallo.puzzle.toyrobot.core.Robot;
 import org.ideallo.puzzle.toyrobot.core.config.Bootstrap;
 import org.ideallo.puzzle.toyrobot.core.enums.DirectionType;
 import org.ideallo.puzzle.toyrobot.core.vector.Coordinates;
 import org.ideallo.puzzle.toyrobot.core.vector.Orientation;
-import org.ideallo.puzzle.toyrobot.service.JoyStickService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -78,6 +67,8 @@ public class JoyStickControllerTest {
 
     @Test
     public void testLeft() throws Exception {
+        resetToNorth();
+
         mvc.perform(put("/joyStick/left").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.directionType", is(DirectionType.WEST.name())))
@@ -103,8 +94,22 @@ public class JoyStickControllerTest {
                 .andExpect(jsonPath("$.coordinates.y", is(0)));
     }
 
+    private void resetToNorth() throws Exception {
+        Orientation orientation  =  new Orientation();
+        orientation.setCoordinates(new Coordinates(0,0));
+        orientation.setDirectionType(DirectionType.NORTH);
+
+        mvc.perform(post("/joyStick/place",orientation).accept(MediaType.APPLICATION_JSON) .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(orientation)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.NORTH.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(0)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+    }
+
     @Test
     public void testRight() throws Exception {
+        resetToNorth();
+
         mvc.perform(put("/joyStick/right").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
@@ -132,11 +137,56 @@ public class JoyStickControllerTest {
 
     @Test
     public void testReport() throws Exception {
+        resetToNorth();
+
+        mvc.perform(put("/joyStick/right").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(0)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
+
+        mvc.perform(put("/joyStick/move").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(1)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
+
+        mvc.perform(put("/joyStick/move").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(2)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
+
+        mvc.perform(put("/joyStick/move").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(3)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
+        mvc.perform(put("/joyStick/move").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(4)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
         mvc.perform(get("/joyStick/report").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("name", is("Akshay Zaveri")))
-                .andExpect(jsonPath("$.directionType", is(DirectionType.NORTH.name())))
-                .andExpect(jsonPath("$.coordinates.x", is(0)))
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(4)))
+                .andExpect(jsonPath("$.coordinates.y", is(0)));
+
+        mvc.perform(put("/joyStick/move").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+
+        mvc.perform(get("/joyStick/report").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name", is("Akshay Zaveri")))
+                .andExpect(jsonPath("$.directionType", is(DirectionType.EAST.name())))
+                .andExpect(jsonPath("$.coordinates.x", is(4)))
                 .andExpect(jsonPath("$.coordinates.y", is(0)));
     }
 
